cb7c29e896e2f6bd726bee21afd3f396a750151f
Oleksandr <111017111+OleksandrHutsul@users.noreply.github.com>
2024-08-09 21:41:10 +0300

    Added testing of two services.

---
+++ TransactionManagement.Tests/UnitTests/LocationServiceTests.cs
@@ -0,0 +1,53 @@
+﻿using System.Net;
+using Moq;
+using Moq.Protected;
+using Newtonsoft.Json.Linq;
+using TransactionManagement.BLL.Services;
+
+namespace TransactionManagement.Tests.UnitTests
+{
+    public class LocationServiceTests
+    {
+        [Fact]
+        public async Task GetLocationInfoAsync_Should_Return_Correct_LocationDTO_For_Given_Coordinates()
+        {
+            // Arrange
+            var geoNamesUsername = "hutsuloleksandr";
+            var expectedResponse = JObject.FromObject(new
+            {
+                timezoneId = "America/Toronto",
+                rawOffset = "-5"
+            }).ToString();
+
+            var httpClientMock = new Mock<HttpMessageHandler>();
+
+            httpClientMock.Protected()
+                .Setup<Task<HttpResponseMessage>>(
+                    "SendAsync",
+                    ItExpr.IsAny<HttpRequestMessage>(),
+                    ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync((HttpRequestMessage request, CancellationToken cancellationToken) =>
+                {
+                    return new HttpResponseMessage
+                    {
+                        StatusCode = HttpStatusCode.OK,
+                        Content = new StringContent(expectedResponse)
+                    };
+                });
+
+            var httpClient = new HttpClient(httpClientMock.Object);
+            var service = new LocationService(httpClient, geoNamesUsername);
+
+            var coordinates = "51.110318592, -77.2466440192";
+
+            // Act
+            var result = await service.GetLocationInfoAsync(coordinates);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Equal("America/Toronto", result.IANAZone);
+            Assert.Equal("-5", result.UTC);
+            Assert.Equal(coordinates, result.Coordinates);
+        }
+    }
+}
---
+++ TransactionManagement.Tests/UnitTests/TransactionServiceTests.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Moq;
+using TransactionManagement.BLL.Services;
+using TransactionManagement.BLL.Interfaces; 
+using TransactionManagement.DAL.Context;
+using TransactionManagement.DTO.Models;
+
+namespace TransactionManagement.Tests.UnitTests
+{
+    public class TransactionServiceTests
+    {
+        [Fact]
+        public async Task UploadingCSVFile_Should_Add_Transaction_To_Database()
+        {
+            // Arrange
+            var options = new DbContextOptionsBuilder<ApiDbContext>()
+                .UseInMemoryDatabase(databaseName: "TestDatabase")
+                .Options;
+
+            using var mockDbContext = new ApiDbContext(options);
+
+            var mockLocationService = new Mock<ILocation>();
+            mockLocationService.Setup(m => m.GetLocationInfoAsync(It.IsAny<string>()))
+                                .ReturnsAsync(new LocationDTO
+                                {
+                                    IANAZone = "America/New_York",
+                                    UTC = "UTC-5",
+                                    Coordinates = "6.602635264, -98.2909591552"
+                                });
+
+            var service = new TransactionService(mockDbContext, mockLocationService.Object);
+
+            var filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..", "..", "..", "TestFiles", "valid_data.csv");
+
+            using var fileStream = new FileStream(filePath, FileMode.Open, FileAccess.Read);
+            // Act
+            await service.UploadingCSVFile(fileStream);
+
+            // Assert
+            var transaction = await mockDbContext.Transactions.FirstOrDefaultAsync();
+            Assert.NotNull(transaction);
+
+            Assert.Equal("T-1-67.63636363636364_0.76", transaction.TransactionId);
+            Assert.Equal("Adria Pugh", transaction.Name);
+            Assert.Equal("odio.a.purus@protonmail.edu", transaction.Email);
+            Assert.Equal(Math.Round(375.39,2), Math.Round(transaction.Amount,2));
+            Assert.Equal(DateTime.Parse("2024-01-10 01:16:23"), transaction.TransactionDate);
+            Assert.Equal("6.602635264, -98.2909591552", transaction.Location.Coordinates);
+        }
+    }
+}
